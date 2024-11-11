cbb9f15fb53b59de54b17d4c3a68b347f8682e87
esedgarcia <samu0850@gmail.com>
2024-11-11 06:46:46 -0500

    feat: add HTTP server for Heroku deployment

--- greetings.go
+++ greetings.go
@@ -1,15 +1,28 @@
-// hola.go
+// greetings.go
 package main
 
 import (
-    "bufio"
     "fmt"
+    "net/http"
     "os"
 )
 
-func main() {
-    reader := bufio.NewReader(os.Stdin)
-    fmt.Print("Escribe tu nombre: ")
-    nombre, _ := reader.ReadString('\n')
-    fmt.Printf("Hola, %s!", nombre)
+func handler(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "Hello, welcome to the Heroku-deployed Go app!")
 }
+
+func main() {
+    http.HandleFunc("/", handler)
+
+    // Use the PORT environment variable on Heroku
+    port := os.Getenv("PORT")
+    if port == "" {
+        port = "8080" // Default port if not set
+    }
+
+    fmt.Printf("Listening on port %s...\n", port)
+    if err := http.ListenAndServe(":"+port, nil); err != nil {
+        fmt.Println("Error starting server:", err)
+    }
+}
+
